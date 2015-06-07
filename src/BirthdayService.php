5a587b43cc6732fa1ca6bc19adf6e974e72a54ab
slaparra <santiago.laparra@atrapalo.com>
2015-06-07 20:43:12 +0200

    Extraemos métodos loadEmployeeData y SendGreeting unitario

--- src/BirthdayService.php
+++ src/BirthdayService.php
@@ -9,21 +9,11 @@ class BirthdayService
 
     public function sendGreetings($fileName, XDate $xDate, $smtpHost, $smtpPort)
     {
-        $fileHandler = fopen($fileName, 'r');
-        fgetcsv($fileHandler);
+        $employees = $this->loadEmployees($fileName);
 
-        $employees = [];
-        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
-            $employeeData = array_map('trim', $employeeData);
-            $employees[] = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
-        }
-        
         foreach ($employees as $employee) {
             if ($employee->isBirthday($xDate)) {
-                $recipient = $employee->getEmail();
-                $body = sprintf('Happy Birthday, dear %s!', $employee->getFirstName());
-                $subject = 'Happy Birthday!';
-                $this->sendMessage($smtpHost, $smtpPort, 'sender@here.com', $subject, $body, $recipient);
+                $this->sendGreeting($smtpHost, $smtpPort, $employee);
             }
         }
     }
@@ -49,5 +39,36 @@ class BirthdayService
     protected function doSendMessage(Swift_Message $msg)
     {
         $this->mailer->send($msg);
+    }
+
+    /**
+     * @param $fileName
+     *
+     * @return array
+     */
+    private function loadEmployees($fileName)
+    {
+        $fileHandler = fopen($fileName, 'r');
+        fgetcsv($fileHandler);
+
+        $employees = [];
+        while ($employeeData = fgetcsv($fileHandler, null, ',')) {
+            $employeeData = array_map('trim', $employeeData);
+            $employees[]  = new Employee($employeeData[1], $employeeData[0], $employeeData[2], $employeeData[3]);
+        }
+
+        return $employees;
+    }
+
+    /**
+     * @param $smtpHost
+     * @param $smtpPort
+     * @param $employee
+     */
+    private function sendGreeting($smtpHost, $smtpPort, $employee)
+    {
+        $body      = sprintf('Happy Birthday, dear %s!', $employee->getFirstName());
+        $subject   = 'Happy Birthday!';
+        $this->sendMessage($smtpHost, $smtpPort, 'sender@here.com', $subject, $body, $employee->getEmail());
     }
 }
